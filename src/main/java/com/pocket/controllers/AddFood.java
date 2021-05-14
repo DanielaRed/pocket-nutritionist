7baf6bc53c37eb2d14d468702dcd604939c4a833
DanielaRed <rednic.dana20@gmail.com>
2021-05-14 18:43:20 +0300

    Added AddFood.fxml

--- src/main/java/com/pocket/controllers/AddFood.java
+++ src/main/java/com/pocket/controllers/AddFood.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import com.pocket.model.Food;
 
 public class AddFood {
 
@@ -17,8 +18,13 @@ public class AddFood {
     @FXML
     private Button Add;
 
-
-
+    Food food_list[] =  {
+            new Food("Bread", "1 slice", 24, 1.5, 5, 130),
+            new Food("Cheese", "1 slice", 4, 7, 6, 100),
+            new Food("Chicken Breast", "100g",2.1,1,22,105),
+            new Food("Banana", "Medium", 27,0.4,1.3,105),
+            new Food("Milk","100 ml",12,0,8,80)
+    };
 
 
 
