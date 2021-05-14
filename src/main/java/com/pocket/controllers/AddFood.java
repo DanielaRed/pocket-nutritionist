edf4f6d6d37b4d4829ff67d499b65baeb4adf773
Stoianovici Alex <alex.stoianovici@gmail.com>
2021-05-14 18:24:24 +0300

    Added food.java

--- src/main/java/com/pocket/controllers/AddFood.java
+++ src/main/java/com/pocket/controllers/AddFood.java
@@ -1,5 +1,17 @@
 package com.pocket.controllers;
 
+import com.pocket.model.Food;
+
 public class AddFood {
+
+    Food food_list[] =  {
+            new Food("Bread", "1 slice", 24, 1.5, 5, 130),
+            new Food("Cheese", "1 slice", 4, 7, 6, 100),
+            new Food("Chicken Breast", "100g",2.1,1,22,105),
+            new Food("Banana", "Medium", 27,0.4,1.3,105),
+            new Food("Milk","100 ml",12,0,8,80)
+    };
+
+
 }
 //
